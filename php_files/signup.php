cbcff2e67167496178f52e95d56f505b6673f88e
Samuel Sarkisian <sarkisiansamuel@gmail.com>
2021-10-26 17:25:06 -0700

    changes to the signup page

--- php_files/signup.php
+++ php_files/signup.php
@@ -14,58 +14,64 @@ session_start();
         $email = mysqli_real_escape_string($con,$_POST['email']);
 		$passwordconfirmed = mysqli_real_escape_string($con,$_POST['passwordconfirmed']);
 
-		if(!empty($username) && !empty($password) && !empty($passwordconfirmed)&& !empty($firstName) && !empty($lastName) && !empty($email) && ($password == $passwordconfirmed)){
-			$sql = "INSERT INTO user (username,password,firstName,lastName,email) VALUES (?, ?, ?, ?, ?);";
-			$stmt = mysqli_stmt_init($con);
-
-
-			if(!mysqli_stmt_prepare($stmt, $sql)){
-				echo("issue...");
-			} 
-			else{
-				$q_username = "SELECT username  FROM user WHERE username ='$username' ";
-				$result_username = mysqli_query($con,$q_username);
-				$num_rows_username = mysqli_num_rows($result_username);
-
-				$q_email = "SELECT  email FROM user WHERE  email = '$email'";
-				$result_email = mysqli_query($con,$q_email);
-				$num_rows_email = mysqli_num_rows($result_email);
-
-				// check for duplicates in database
-				if(!$num_rows_username && !$num_rows_email)
-				{
-				//save to database
-				
-				mysqli_stmt_bind_param($stmt, "sssss", $username, $password, $firstName, $lastName, $email);
-				mysqli_stmt_execute($stmt);
-				header("Location: login.php");
-				die;
-				}
-
-				elseif($num_rows_username && $num_rows_email)
-				{
-					echo" Duplicate email and username please try again";
-				}
-
-				elseif($num_rows_username)
-				{
-					echo" Duplicate username please try again";
-				}
-
-				elseif($num_rows_email)
-				{
-					echo" Duplicate email please try again";
-				}
-
-
+		if(!empty($username) && !empty($password) && !empty($passwordconfirmed)&& !empty($firstName) && !empty($lastName) && !empty($email)){
+			
+			if ($password != $passwordconfirmed){
+				echo("password and passwordconfirmed does not match, please try again");
 				
 			}
+			else{
+				$sql = "INSERT INTO user (username,password,firstName,lastName,email) VALUES (?, ?, ?, ?, ?);";
+				$stmt = mysqli_stmt_init($con);
 
-		
+
+				if(!mysqli_stmt_prepare($stmt, $sql)){
+					echo("issue...");
+				} 
+				else{
+					$q_username = "SELECT username  FROM user WHERE username ='$username' ";
+					$result_username = mysqli_query($con,$q_username);
+					$num_rows_username = mysqli_num_rows($result_username);
+
+					$q_email = "SELECT  email FROM user WHERE  email = '$email'";
+					$result_email = mysqli_query($con,$q_email);
+					$num_rows_email = mysqli_num_rows($result_email);
+
+					// check for duplicates in database
+					if(!$num_rows_username && !$num_rows_email)
+					{
+					//save to database
+					
+					mysqli_stmt_bind_param($stmt, "sssss", $username, $password, $firstName, $lastName, $email);
+					mysqli_stmt_execute($stmt);
+					header("Location: login.php");
+					exit;
+					}
+
+					elseif($num_rows_username && $num_rows_email)
+					{
+						echo" Duplicate email and username please try again";
+					}
+
+					elseif($num_rows_username)
+					{
+						echo" Duplicate username please try again";
+					}
+
+					elseif($num_rows_email)
+					{
+						echo" Duplicate email please try again";
+					}
+
+
+					
+				}
+
+			}
 		}
 		else
 		{
-			echo "Empty slot for user input, please fill in!";
+			echo "Empty slot for user input, please fill in";
 		}
 		
 	}
